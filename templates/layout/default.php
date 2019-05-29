3bdd30c60596f2d09313dcf68f057a228be13e09
David Yell <dyell@ukwebmedia.com>
2019-05-29 16:10:28 +0100

    Load free Fontawesome

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -9,6 +9,7 @@
     <?= $this->Html->meta('icon') ?>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 
     <?= $this->fetch('meta') ?>
     <?= $this->Html->css(['site']);?>
